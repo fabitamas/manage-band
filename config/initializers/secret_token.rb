ee1fcec82b92e48c8f93be3e94cae2f9e062c2d7
Tamas Fabian <fabitamas@gmail.com>
2015-06-15 22:03:43 +0200

    Initial rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManageBand::Application.config.secret_token = '672394921df5819c95edcc06bac1baeccc7e7558798ac4410ae781b1b584e76b9660d4834e23dd1672d01c8c8276762041be6ac04d30c733312e8764bd30e00e'
