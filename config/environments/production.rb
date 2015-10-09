1c3542d3e510d7a08153bb211616827e02927910
Tamas Fabian <fabitamas@gmail.com>
2015-10-09 11:44:27 +0200

    playing with asset versions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ ManageBand::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.version = 2
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
